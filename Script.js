cced08fb606531247caf4507efc33d00810d4425
Vivek Kulkarni <vivekkulkarni3013@example.com>
2025-08-13 19:16:09 +0530

    colorChanger

---
+++ Script.js
@@ -0,0 +1,23 @@
+const body = document.querySelector("body");
+const buttons = document.querySelectorAll(".button");
+
+buttons.forEach(function (button) {
+   button.addEventListener('click', function(e) {
+console.log(e.target);
+
+    if(e.target.id === "grey"){
+        body.style.backgroundColor = e.target.id ;
+    }
+    if(e.target.id === "white"){
+        body.style.backgroundColor = e.target.id ;
+    }
+    if(e.target.id === "blue"){
+        body.style.backgroundColor = e.target.id ;
+    }
+    if(e.target.id === "yellow"){
+        body.style.backgroundColor = e.target.id ;
+    }
+  
+   });
+
+ });
